b03445f7ddec91bc8c36ada53280c1803fc1166f
Lujanhi <51241524+Lujanhi@users.noreply.github.com>
2019-07-24 10:59:38 -0400

    save 1

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+// https://teamtreehouse.com/library/asynchronous-programming-with-javascript/what-is-asynchronous-programming/the-javascript-call-stack
+//The JavaScript Call Stack
+
+//https://teamtreehouse.com/library/asynchronous-programming-with-javascript/what-is-asynchronous-programming/the-callback-queue-and-event-loop
+//The Callback Queue and Event Loop
+
+//https://teamtreehouse.com/library/asynchronous-programming-with-javascript/asynchronous-javascript-with-callbacks/callbacks-review
+//Callbacks Review
+
+
+
+//https://teamtreehouse.com/library/create-a-promise
+// Create a Promise   then()  catch() resolve()
+
+// https://teamtreehouse.com/library/reject-a-promise-and-handle-errors
+// Reject a Promise and Handle Errors    catch()  reject ()
+
+//https://teamtreehouse.com/library/asynchronous-programming-with-javascript/understanding-promises/promises-review
+// Promises Review  there is code to copy
+
+//From Callbacks to Promises
+//https://teamtreehouse.com/library/from-callbacks-to-promises
+
+
+
+//https://teamtreehouse.com/library/handle-multiple-promises-with-promiseall
+/* const func1 = getJSON('...');
+const func2 = getJSON('...');
+
+Promise.all([func1, func2])
+  .then(results => {
+    console.log('Array of results', results);
+  })
+  .catch(error => {
+    console.error(error);
+  }) */
